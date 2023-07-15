b055eeda6f35943eb4b9a2048625aa2c25bcd033
Sinthuja Nagalingam <sinthuja.nagalingam@sanabenefits.com>
2023-07-15 11:37:57 -0700

    get basic page and chatgpt functionality working

---
+++ pages/api/gpt4.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Configuration, OpenAIApi } = require('openai');
+let openai;
+
+export default async function handler(req, res) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  openai = new OpenAIApi(configuration);
+
+  try {
+    const userPrompt = req.body.userPrompt;
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0.888,
+        max_tokens: 2048,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        top_p: 1,
+        messages: [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": userPrompt},
+        ]
+    }, { timeout: 60000 });
+
+    res.status(200).json({ answer: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+}
